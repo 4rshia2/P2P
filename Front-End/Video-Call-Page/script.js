93ad76c65133167d4d5624487f97c897076412cd
Aminzein <Aminzein@users.Zforce.github.com>
2020-09-12 22:24:39 +0430

    generating id handled

--- Front-End/Video-Call-Page/script.js
+++ Front-End/Video-Call-Page/script.js
@@ -1,7 +1,18 @@
-function myFunction() {
+const uuidGenerator = require("./uuid-Generator");
+function copyRoomId() {
   var copyText = document.getElementById("myInput");
   copyText.select();
   copyText.setSelectionRange(0, 99999)
   document.execCommand("copy");
   alert("Copied the text: " + copyText.value);
 }
+
+let generateIdButton = document.getElementsByClassName("generateId");
+if(generateIdButton){
+	console.log("its ok");
+}
+generateIdButton.addEventListener("click" , () => {
+	let roomId = uuidGenerator();
+	inputBar = document.getElementById("myInput");
+	inputBar.value = roomId;
+})
--- uuid-Generator.js
+++
@@ -1,6 +0,0 @@
-var uuid = require('uuid')
-
-module.exports = function(){
-    var uniqueID = uuid.v4();
-    return uniqueID;
-}
